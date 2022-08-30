07c816a5ae87682aa4563bc2a6172dab519f06ff
telecomjorgeandres <jorgebch@localhost.localdomain>
2022-08-30 16:07:01 -0400

    Edit README.md and test1.py

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # test_python
+Introduccion a Python, en 100 dias intentare codificar y aprender con diversas tareas y ejercios, las funciones basicas de Python. I can do it!
---
+++ test1.py
@@ -0,0 +1,16 @@
+inicio = "Bienvenido a Python con Jorge"
+print(inicio)
+
+nombre = input("Introduce tu nombre:\n")
+
+print("Hola " + nombre) #nombre
+
+edad = input("Introduce tu edad:\n")
+
+print("Tu edad es " +  edad) #edad
+
+lugar = input("De donde eres:\n")
+
+print("Eres nativo de " + lugar) #lugar
+
+print("Hola " +  nombre + ", tienes " + edad + " y eres de " + lugar)
